c1f5fa7d94c6d2d02eb81be18ea7eb705733f816
igormos1 <gior_gt@ukr.net>
2022-06-04 22:24:09 +0300

    igormos1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,43 @@
-// import { useState,useEffect } from "react";
-import ContactForm from "./ContactForm/ContactForm";
-import Filter from "./Filter/Filter";
-import ContactList from "./ContactList/ContactList";
+import { Routes, Route} from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+// import { RegisterView } from 'views/registerfView';
+// import { LogInView } from 'views/logInView';
+// import { ContactsView } from 'views/contactsView';
+import Container from 'App.style';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import authOperations from './redux/auth/auth-operations';
+import PrivateRoute from './components/Routes/PrivateRoute'
+import PublicRoute from 'components/Routes/PublicRoute';
+import AppBar from 'components/AppBar/AppBar';
+// import { HomeView } from 'views/homeView';
 
-export function App() {
+const LogInView = lazy(()=> import('./views/logInView'));
+const RegisterView = lazy(()=> import('./views/registerfView'));
+const ContactsView = lazy(()=> import('./views/contactsView'));
+const HomeView = lazy(()=> import('./views/homeView'));
+const NotFoundView = lazy(()=> import('./views/notFoundView'))
+
+export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
+
   return (
-    <div>
-    <h1>Phonebook</h1>
-      <ContactForm/>
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactList/>
-    </div>
-  )
+    <Container>
+      <Suspense fallback={<p>Loading...</p>}>
+       <Routes>
+         <Route path='/' element = {<AppBar/>}>
+           <Route index element = {<HomeView/>}/>
+            <Route path='register' element ={ <PublicRoute>{<RegisterView/>}</PublicRoute>}/>
+           <Route path='login' element ={<PublicRoute>{<LogInView/>}</PublicRoute>}/>
+              <Route path ='contacts' element={<PrivateRoute>{<ContactsView/>}</PrivateRoute>}/>
+         </Route>
+         <Route path="*" element={<NotFoundView/>} /> 
+       </Routes>
+       </Suspense>
+    </Container>
+  );
 };
-
